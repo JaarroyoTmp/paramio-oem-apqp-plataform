3afad3e465b6ce001fbd6b9b73aeab91a405f625
unknown <jaarroyo@tmparamio.com>
2026-05-21 10:49:57 +0200

    Add isolated Supabase and engine tests

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://nwxioucoddtrditemdwg.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://nwxioucoddtrditemdwg.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53eGlvdWNvZGR0cmRpdGVtZHdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyODQ5NDQsImV4cCI6MjA5NDg2MDk0NH0.WTQRPSH-qB7eWc3ZHamiNiMcgU7bg78jFG7qUXmC56w';
 
 const supabaseClient = supabase.createClient(
